279d78812910fbcf900c2f1aea7a074b67bcbfb2
sirucekBM <bmartins@seznam.cz>
2022-09-17 19:35:35 +0200

    vyladeni masky

--- model1.py
+++ model1.py
@@ -56,23 +56,25 @@ def userContrast(img):
 #maska obrázku
 def userMask(imgOrig, img):
 
-    low_green = (20, 115, 15)#(28, 81, 27)tmavě zelená
-    high_green = (140, 255,230)# (140, 255,230)
+    low_green = (20, 115, 15)#(20, 115, 15)tmavě zelená
+    high_green = (140, 255,218)# (140, 255,230) # 24,158,17
 
+
+    #low_green = (37, 21, 0)#(20, 115, 15)tmavě zelená
+    #high_green = (74, 228,255)# (140, 255,230) # 24,158,17
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)       
     mask = cv2.inRange(hsv, low_green , high_green)
 
+
     result = cv2.bitwise_and(imgOrig,imgOrig, mask=mask)
-
-    imask = mask>0
-    green = np.zeros_like(imgOrig, np.uint8)
-    green[imask] = imgOrig[imask]
-
     greenMask = cv2.GaussianBlur(result, (5, 5), 1)
+
+    #cv2.imshow('img greenMask', greenMask)
+    #cv2.waitKey(0)
 
     return greenMask
 
-
+#vrátí výřez obrázku podle nejdelší hrany
 def getMaxEdge(imgMask,imgOrig):
 
     tLower = 125  # nizky prahLower Threshold
@@ -94,6 +96,8 @@ def getMaxEdge(imgMask,imgOrig):
     x,y,w,h= cv2.boundingRect(contour)#nakreslím obdelník v nalezené max kontuře
 
     cropped_contour= imgOrig[y:y+h, x:x+w]
+    #cv2.imshow("Contours", cropped_contour)
+    #cv2.waitKey(0)
 
     return cropped_contour
 
